d212de7b77197907d06903492dbaad72b4f8745e
xsmops <roman.r.s2005@gmail.com>
2021-08-30 01:40:04 +0300

    Added static files

---
+++ static/script/script.js
@@ -0,0 +1,18 @@
+
+const anchors = document.querySelectorAll("a.nav-item")
+
+for(let anchor of anchors) {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        const blockId = anchor.getAttribute('href').substr(1);
+
+
+        document.getElementById(blockId).scrollIntoView({
+
+            behavior: 'smooth',
+            block: 'start',
+
+        })
+    })
+}
